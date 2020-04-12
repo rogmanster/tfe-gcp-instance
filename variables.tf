2cb329f4fac365613720f00227fa22327ed8efc9
Roger Chao <rogman@gmail.com>
2020-04-11 17:50:26 -0700

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,33 +1,7 @@
-variable "gcp_credentials" {
-  description = "GCP credentials needed by google provider"
-}
-
-variable "gcp_project" {
-  description = "GCP project name"
-}
-
-variable "gcp_region" {
-  description = "GCP region, e.g. us-east1"
-  default     = "us-west2"
-}
-
-variable "gcp_zone" {
-  description = "GCP zone, e.g. us-east1-a"
-  default     = "us-west2-b"
-}
-
-variable "machine_type" {
-  description = "GCP machine type"
-  default     = "n1-standard-1"
-}
-
-variable "instance_name" {
-  description = "GCP instance name"
-  default     = "rogmanster"
-}
-
-variable "image" {
-  description = "image for you"
-  default     = "ubuntu-minimal-1810-cosmic-v20181119a"
-}
-
+variable "gcp_credentials" {}
+variable "gcp_project" {}
+variable "gcp_region" {}
+variable "gcp_zone" {}
+variable "machine_type" {}
+variable "instance_name" {}
+variable "image" {}
